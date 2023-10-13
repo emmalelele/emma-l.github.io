29270e00966f727fa550ecc54b47d0aac0c29e20
Emma <115744148+emmalelele@users.noreply.github.com>
2023-10-13 12:17:59 -0600

    get 5 random cards from the 52 cards with notations

--- arrays assignment/sketch.js
+++ arrays assignment/sketch.js
@@ -1,4 +1,5 @@
 // Emma Le
+// Arrays and object notation assignment
 
 let numsCard = 5;
 
@@ -7,6 +8,17 @@ const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"];
 //clubs, diamonds, heart, spade
 const suits = ["C", "D", "H", "S"];
 
+
+
+
+
+function setup(){
+	createCanvas(windowWidth, windowHeight); 
+}
+
+function draw(){
+	background("black");
+}
 
 
 function createCards() {
@@ -23,4 +35,32 @@ function createCards() {
 	}
 	return cards;
 }
+console.log(createCards())
+
+// get random
+function shuffleCards(array){
+	let i = 0 ;
+	let j = 0;
+	let store = 0;
+	for (i = array.length - 1; i > 0; i --){
+		j = Math.floor(Math.random()*i);  //get the random elements from 0 to i - 1 
+		store = array[i];
+		array[i] = array[j];
+		array[j] = store;
+	}
+	return array;
+}
+
+let randomCard = shuffleCards(createCards());
+console.log(randomCard);
+
+
+function takeOutCards(){
+	let yourCards = [];
+	for (i= 0; i < numsCard; i++){
+		store = yourCards.push(randomCard.pop()) //take out the last 5 cards 
+	}
+	return yourCards;
+}
+console.log(takeOutCards());
 
