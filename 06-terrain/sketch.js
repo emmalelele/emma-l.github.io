e4ffaba6baa9379d09ea21d03b472910ad605583
Emma <115744148+emmalelele@users.noreply.github.com>
2023-10-23 12:17:11 -0600

    updates

--- 06-terrain/sketch.js
+++ 06-terrain/sketch.js
@@ -2,6 +2,8 @@
 // Oct 23 2023
 
 let terrain = [];
+let xOffset = 0;
+let xMax = 9000;
 
 
 
@@ -13,18 +15,28 @@ function setup() {
 function draw() {
   background(220);
   displayRect();
+  if (keyIsDown(RIGHT_ARROW)){
+    if (xOffset < xMax){ //dont fall off the right side
+      xOffset += 10;
+    }
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    if (xOffset > 5){ //dont fall off the left side
+      xOffset -= 10;
+    }
+  }
 }
 
 function displayRect(){
-  for (let i = 0; i < width; i ++){
+  for (let i = xOffset; i < width + xOffset; i ++){
     let thisRect = terrain[i];
-    rect(thisRect.x , height - thisRect.height, 1, thisRect.height)
+    rect(thisRect.x - xOffset, height - thisRect.height, 1, thisRect.height)
   }
 }
 
 function spawnRect(){
   let time = 0;
-  for (let x = 0; x < 10000; x++){
+  for (let x = 0; x < xMax; x++){
     let h = noise(time) * height;
     let thisRect = {
       x: x,
